3431b9a1cf5ab5975a46266c4fb5a4f7e6b7885d
MatrixCompSci <wirezapp@gmail.com>
2014-11-18 16:41:52 -0500

    Deleted unneeded files

--- Editor+Engine/AI_Sample/Lib/FFT/stdafx.h
+++
@@ -1,9 +0,0 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
-
-#pragma once
-//#define AI_DEBUG_MODE_GOALS //uncomment this to  see debugging information
-//#define WORLD_BREAK_POINT //uncomment this to add pausing with simulation
-// TODO: reference additional headers your program requires here
